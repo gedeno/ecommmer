fde989570a28d1648b3f7beda21a194dd8d6414f
gedeno <gedenogeremew@gmail.com>
2026-03-30 21:25:28 +0300

    new sign up page is created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login ,logout, authenticate
+from django.contrib.auth.forms import UserCreationFrom
+from django.contrib.auth.models import User
 
 # Create your views here.
+def registration(requeset):
+    form = UserCreationFrom()
+    if requeset.method == "post":
+        form = UserCreationFrom(requeset.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
