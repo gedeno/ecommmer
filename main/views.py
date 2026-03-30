7d3b96f095ef166967af3169dd49f9483ac7d53f
gedeno <gedenogeremew@gmail.com>
2026-03-30 22:11:36 +0300

    maigrations are done

--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login ,logout, authenticate
-from django.contrib.auth.forms import UserCreationFrom
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Create your views here.
 def registration(request):
-    form = UserCreationFrom()
+    form = UserCreationForm()
     if request.method == "POST":
-        form = UserCreationFrom(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
