d11adc41ba2290ee0432549c863cb6b6afefd6ce
gedeno <gedenogeremew@gmail.com>
2026-03-30 22:03:55 +0300

    new login page is created

--- main/views.py
+++ main/views.py
@@ -5,11 +5,25 @@ from django.contrib.auth.forms import UserCreationFrom
 from django.contrib.auth.models import User
 
 # Create your views here.
-def registration(requeset):
+def registration(request):
     form = UserCreationFrom()
-    if requeset.method == "post":
-        form = UserCreationFrom(requeset.POST)
+    if request.method == "POST":
+        form = UserCreationFrom(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
+    return render(request,'main/register.html',{'form':form})
+def logins(request):
+    if request.method == 'POST':
+        Username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=Username,password=password)
+        if user != None:
+            login(request.user)
+            return redirect('home')
+    return render(request,'main/login.html')
+@login_required(login_url='login/')
+def home(request):
+    return render(request,'main/home.html')
+
 
